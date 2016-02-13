d7b9444caca5a94834267f74c3028466e9dd876a
Robin Houtevelts <robin@houtevelts.com>
2016-02-13 03:00:42 +0100

    Update cli.js

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ var input = fs.readFileSync("./"+cli[0], 'utf8');
 input = input
   .replace(/([\d]{2}:[\d]{2}:[\d]{2}.[\d]{3} -->)/g, "ROBINISCOOL\r\n$1")
   .replace(/([\d]{2}:[\d]{2}:[\d]{2}.[\d]{3})\r\n\r\n/g, "$1\r\n")
-  .replace(/WEBVTT.*$\r\n\r\n/gm, '')
+  .replace(/WEBVTT[\s\S]*?[\d]{2}:[\d]{2}:[\d]{2}.[\d]{3}/gm, "$1")
   .replace(/ROBINISCOOL\r\n00:00:00.000 --> 00:00:00.000[\s\S]*?ROBINISCOOL/gm, "ROBINISCOOL")
   .replace(/([\d]{2}:[\d]{2}:[\d]{2})\.([\d]{3})/g, "$1,$2")
   .replace(/ROBINISCOOL/g, function(s){
